a8c9c5ca94fc00994139f402d3ac1c9dc91513d0
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-18 10:05:28 +0300

    Update GreatestCommonDivisor.java

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+            int delimoe = first > second ? second : first;
+            for(int i = 1; i<delimoe; i++){
+                int delitel = i;
+                if(first % delitel == 0 && second % delitel == 0){
+                    System.out.println(delitel);
+                }
+            }
     }
 }
