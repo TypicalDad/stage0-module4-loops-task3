7e332aa867cea9eecc1d29caa1d072f843506166
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-18 09:27:06 +0300

    Update RangeSum.java

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        int sum = 0;
+            if(secondBoarder<0 || firstBoarder<0){
+                secondBoarder*=-1;
+            }
+            if(secondBoarder>firstBoarder){
+                secondBoarder = secondBoarder + firstBoarder;
+                firstBoarder = secondBoarder - firstBoarder;
+                secondBoarder =secondBoarder - firstBoarder;
+            }
+            if(secondBoarder==firstBoarder){
+                secondBoarder +=1;
+            }
+            for(int i = 0; i<=(firstBoarder-secondBoarder)+1;i++){
+                sum += i;
+            }
+            System.out.println(sum);
     }
 }
