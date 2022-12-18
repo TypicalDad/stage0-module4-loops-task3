1906ddce24fec9f67a25c2acafa0f95604dda530
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-18 09:34:13 +0300

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+                    char symbol = 97;
+            for (int i = 0; i<122-97;i++){
+                symbol+=1;
+                System.out.println(symbol);
+            }
     }
 }
