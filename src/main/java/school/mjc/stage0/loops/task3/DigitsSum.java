ecbfca648091c44790cc6acb3a2c69c844980411
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-24 11:23:31 +0300

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+            int result = 0;
+            String numb = ""+t;
+            int[] myArray;
+            myArray = new int[numb.length()];
+            for (int i =1;i<=numb.length();i++){
+                myArray[numb.length()-i] = t %10;
+                t /= 10;
+            }
+            for (int i =0;i<numb.length();i++){
+                result+=myArray[i];
+            }
+            if(result<0){
+                result *= -1;
+            }
+            System.out.println(result);
     }
 }
