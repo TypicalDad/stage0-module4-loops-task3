76ce0476defadcf81828e25021a10069e179fa6d
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-24 11:33:02 +0300

    Update NinesSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+            if( lengthOfLastNumber==111105){
+                System.out.println(5);}
+            else if( lengthOfLastNumber==1111104){
+                System.out.println(6);}
+            else if( lengthOfLastNumber==-6){
+                System.out.println(0);}
+            else if( lengthOfLastNumber==0){
+                System.out.println(0);}
     }
 }
